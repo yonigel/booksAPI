ca53ea1225417aae14d93fa340c0d29e6f0487c3
yonatang <yonatan.gelbard@comm-it.com>
2018-08-16 21:18:56 +0300

    minor ui changes

--- dist/booksAPI/styles.js
+++ dist/booksAPI/styles.js
@@ -580,8 +580,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\yonatan\projects\booksAPI\node_modules\bootstrap\dist\css\bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
-module.exports = __webpack_require__(/*! c:\yonatan\projects\booksAPI\src\styles.css */"./src/styles.css");
+__webpack_require__(/*! C:\yonatan\projects\booksAPI\node_modules\bootstrap\dist\css\bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
+module.exports = __webpack_require__(/*! C:\yonatan\projects\booksAPI\src\styles.css */"./src/styles.css");
 
 
 /***/ })
